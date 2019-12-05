448a2bc83013c4c32cd8fb275165aa7c5ce80a2e
Administrator <510510280@qq.com>
2019-12-05 13:51:47 +0800

    我的第一个tornado程序

---
+++ 01.py
@@ -0,0 +1,60 @@
+import tornado.web
+import tornado.ioloop
+
+
+class IndexHandler(tornado.web.RequestHandler):
+    '''
+    主页处理函数
+    '''
+    def get(self):
+        self.write("hello tornado!")
+
+
+if __name__ == '__main__':
+    app = tornado.web.Application([(r"/",IndexHandler)])
+    app.listen(8000)
+    tornado.ioloop.IOLoop.current().start()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
+
+       ┌─┐       ┌─┐ + +
+    ┌──┘ ┴───────┘ ┴──┐++
+    │                 │
+    │       ───       │++ + + +
+    ███████───███████ │+
+    │                 │+
+    │       ─┴─       │
+    │                 │
+    └───┐         ┌───┘
+        │         │
+        │         │   + +
+        │         │
+        │         └──────────────┐
+        │                        │
+        │                        ├─┐
+        │                        ┌─┘
+        │                        │
+        └─┐  ┐  ┌───────┬──┐  ┌──┘  + + + +
+          │ ─┤ ─┤       │ ─┤ ─┤
+          └──┴──┘       └──┴──┘  + + + +
+                 神兽保佑
+                代码无BUG!
+
+
+'''
