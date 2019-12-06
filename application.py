75faa4da007a8a2e72f17daaa86840524a49dc11
Administrator <510510280@qq.com>
2019-12-06 15:16:54 +0800

    通过url的名字进行页面中连接的跳转

--- application.py
+++ application.py
@@ -10,6 +10,13 @@ class Application(tornado.web.Application):
 
             # 状态码
             (r"/status", index.StatusHandler),
+            # 重定向
+            (r"/index", index.RedirectHandler),
+            # 错误处理
+            # iserror?flag=2
+            # 如果等于0就说明,有错误,不等于0就说明没有错误
+            (r"/iserror", index.ErrorHandler),
 
+            tornado.web.url(r"/kaige",index.KaigeHandler,name='kaige'),
         ]
         super(Application,self).__init__(handlers)
--- views/index.py
+++ views/index.py
@@ -1,14 +1,20 @@
+from typing import Any
+
 from tornado.web import RequestHandler
 
 
 class IndexHandler(RequestHandler):
     def get(self):
         self.write("main page info tornado!")
+        self.write("<br>")
+        url = self.reverse_url("kaige")
+        self.write("<a href='%s'>去另一个页面</a>" % (url))
+        # self.write("<a href="+url+">去另一个页面</a>")
 
 
 class SunckHandler(RequestHandler):
     # 该方法会在HTTP方法之前调用
-    def initialize(self,age,name) -> None:
+    def initialize(self, age, name) -> None:
         self.age = age
         self.name = name
 
@@ -21,6 +27,41 @@ class SunckHandler(RequestHandler):
 class StatusHandler(RequestHandler):
 
     def get(self):
-        self.set_status(999,"我是谁,我在哪,我要干神马")
+        self.set_status(999, "我是谁,我在哪,我要干神马")
         self.write("status page info tornado!")
 
+
+class RedirectHandler(RequestHandler):
+
+    def get(self):
+        # 直接就重定向了
+        self.redirect("/")
+
+
+class ErrorHandler(RequestHandler):
+
+    def write_error(self, status_code: int, **kwargs: Any) -> None:
+        if status_code == 500:
+            self.write("服务器内部错误500了")
+        elif status_code == 404:
+            self.write("资源不存在")
+        else:
+            self.write("我也不知道是啥错误")
+
+    def get(self):
+        # 直接就重定向了
+        flag = self.get_query_argument("flag")
+        if flag == '0':
+            print("有错误")
+            self.send_error(500)
+            # 这里抛出错误,下面就不会执行了
+
+        print("没毛病")
+
+        self.write("you are right!")
+
+
+class KaigeHandler(RequestHandler):
+
+    def get(self):
+        self.write("hansklg kaige !")
