e0fe92a4b545287d8850ca86ac964c7e7047c65b
Administrator <510510280@qq.com>
2019-12-09 15:32:40 +0800

    开始同步异步demo

--- database_operation/mydb_students.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO mydb.students (name, age, sex) VALUES ('张三', 15, '男');
-INSERT INTO mydb.students (name, age, sex) VALUES ('李四', 18, '女');
-INSERT INTO mydb.students (name, age, sex) VALUES ('赵柳', 24, '男');
-INSERT INTO mydb.students (name, age, sex) VALUES ('知乎', 19, '男');
-INSERT INTO mydb.students (name, age, sex) VALUES ('Nancy', 21, '女');
-INSERT INTO mydb.students (name, age, sex) VALUES ('小强', 23, '未知');
--- database_operation/students.sql
+++
@@ -1,8 +0,0 @@
-create table mydb.students
-(
-    name varchar(1020) null,
-    age  int           null,
-    sex  tinytext      null
-);
-
-
