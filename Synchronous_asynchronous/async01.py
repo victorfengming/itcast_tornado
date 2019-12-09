e8d540ad3fb1dbb239b7dbe92b5106b4472921d4
Administrator <510510280@qq.com>
2019-12-09 16:29:00 +0800

    利用协程写的异步操作

--- Synchronous_asynchronous/async01.py
+++ Synchronous_asynchronous/async01.py
@@ -10,30 +10,34 @@
 import time
 import threading
 
+gen = None
+
 # 添加一个耗时的操作
-def longIO(callback):
-    def run(cb):
+def longIO():
+    def run():
         print("开始耗时操作")
         time.sleep(3)
+        try:
+            global gen
+            gen.send("victor is wonderful!!!")
+        except StopIteration as e:
+            pass
+
         print("结束耗时操作")
-        cb("victor is a wonderful man")
     threading.Thread(
         target=run,
-        args=(callback,)
     ).start()
 # 这个longio这部分 ,就像ajax一样都不用我们来写了
 '''
 这样就会有一个问题,这个run()函数的返回值我们接受不到
 为了解决这个问题,我们需要写一个函数,这个函数叫做回调函数
 '''
-def finish(data):
-    print("开始处理回调函数")
-    print("接受到longIO的数据为：",data)
-    print("结束处理回调函数")
 
 def reqA():
     print("开始处理reqA")
-    longIO(finish)
+    res = yield longIO()
+    print("接受到longIO的数据为：",res)
+    # 这里就相当于挂起了
     print("结束处理reqA")
 
 
@@ -45,7 +49,9 @@ def reqB():
 
 def main():
     # 这就是同步在处理
-    reqA()
+    global gen
+    gen = reqA()    # 生成一个生成器
+    next(gen)   # 执行reqA
     reqB()
     while True:
         '''
